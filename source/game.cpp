95c04c3992aa7c5bf3b69a29605990ab5f13fe89
Aras Pranckevicius <aras@unity3d.com>
2018-09-24 07:12:03 +0300

    Rework GameObject/Component system; each GameObject just contains data members for each possible component. No longer need a base Component class. - 100000,20: 6.2ms update, 13.5ms start - 1000000,20: 43.2ms update, 112.4ms start, 218MB

--- source/game.cpp
+++ source/game.cpp
@@ -14,139 +14,19 @@ static float RandomFloat(float from, float to) { return RandomFloat01() * (to - 
 
 
 // -------------------------------------------------------------------------------------------------
-// super simple "component system"
-
-class GameObject;
-class Component;
-
-typedef std::vector<Component*> ComponentVector;
-typedef std::vector<GameObject*> GameObjectVector;
-
-// C++ dynamic_cast is fairly slow, so roll our own super simple "type identification" system,
-// where each component stores an enum for "what type I am?"
-enum ComponentType
-{
-    kCompPosition,
-    kCompSprite,
-    kCompWorldBounds,
-    kCompMove,
-};
-
-// Component base class. Knows about the parent game object, and has some virtual methods.
-class Component
-{
-public:
-    virtual ~Component() {}
-    
-    virtual void Start() {}
-
-    const GameObject& GetGameObject() const { return *m_GameObject; }
-    GameObject& GetGameObject() { return *m_GameObject; }
-    void SetGameObject(GameObject& go) { m_GameObject = &go; }
-    bool HasGameObject() const { return m_GameObject != nullptr; }
-    
-    ComponentType GetType() const { return m_Type; }
-
-protected:
-    Component(ComponentType type) : m_GameObject(nullptr), m_Type(type) {}
-
-private:
-    GameObject* m_GameObject;
-    ComponentType m_Type;
-};
-
-
-// Game object class. Has an array of components.
-class GameObject
-{
-public:
-    GameObject(const std::string&& name) : m_Name(name) { }
-    ~GameObject()
-    {
-        // game object owns the components; destroy them when deleting the game object
-        for (auto c : m_Components) delete c;
-    }
-
-    // get a component of type T, or null if it does not exist on this game object
-    template<typename T>
-    T* GetComponent()
-    {
-        for (auto c : m_Components)
-        {
-            if (c->GetType() == T::kTypeId)
-                return (T*)c;
-        }
-        return nullptr;
-    }
-
-    // add a new component to this game object
-    void AddComponent(Component* c)
-    {
-        assert(!c->HasGameObject());
-        c->SetGameObject(*this);
-        m_Components.emplace_back(c);
-    }
-    
-    void Start() { for (auto c : m_Components) c->Start(); }
-    
-private:
-    std::string m_Name;
-    ComponentVector m_Components;
-};
-
-// The "scene": array of game objects.
-static GameObjectVector s_Objects;
-
-
-// Finds all components of given type in the whole scene
-template<typename T>
-static ComponentVector FindAllComponentsOfType()
-{
-    ComponentVector res;
-    for (auto go : s_Objects)
-    {
-        T* c = go->GetComponent<T>();
-        if (c != nullptr)
-            res.emplace_back(c);
-    }
-    return res;
-}
-
-// Find one component of given type in the scene (returns first found one)
-template<typename T>
-static T* FindOfType()
-{
-    for (auto go : s_Objects)
-    {
-        T* c = go->GetComponent<T>();
-        if (c != nullptr)
-            return c;
-    }
-    return nullptr;
-}
-
-
-
-// -------------------------------------------------------------------------------------------------
-// components we use in our "game"
+// components we use in our "game". these are all just simple structs with some data.
 
 
 // 2D position: just x,y coordinates
-struct PositionComponent : public Component
+struct PositionComponent
 {
-    enum { kTypeId = kCompPosition };
-    PositionComponent() : Component((ComponentType)kTypeId) {}
-    
     float x, y;
 };
 
 
 // Sprite: color, sprite index (in the sprite atlas), and scale for rendering it
-struct SpriteComponent : public Component
+struct SpriteComponent
 {
-    enum { kTypeId = kCompSprite };
-    SpriteComponent() : Component((ComponentType)kTypeId) {}
-
     float colorR, colorG, colorB;
     int spriteIndex;
     float scale;
@@ -154,23 +34,18 @@ struct SpriteComponent : public Component
 
 
 // World bounds for our "game" logic: x,y minimum & maximum values
-struct WorldBoundsComponent : public Component
+struct WorldBoundsComponent
 {
-    enum { kTypeId = kCompWorldBounds };
-    WorldBoundsComponent() : Component((ComponentType)kTypeId) {}
-    
     float xMin, xMax, yMin, yMax;
 };
 
 
 // Move around with constant velocity. When reached world bounds, reflect back from them.
-struct MoveComponent : public Component
+struct MoveComponent
 {
-    enum { kTypeId = kCompMove };
-    
     float velx, vely;
 
-    MoveComponent(float minSpeed, float maxSpeed) : Component((ComponentType)kTypeId)
+    void Initialize(float minSpeed, float maxSpeed)
     {
         // random angle
         float angle = RandomFloat01() * 3.1415926f * 2;
@@ -180,34 +55,67 @@ struct MoveComponent : public Component
         velx = cosf(angle) * speed;
         vely = sinf(angle) * speed;
     }
-    
-    virtual void Start() override;
 };
+
+
+// -------------------------------------------------------------------------------------------------
+// super simple "game object system". each object has data for all possible components,
+// as well as flags indicating which ones are actually present.
+
+struct GameObject
+{
+    GameObject(const std::string&& name) : m_Name(name), m_HasPosition(0), m_HasSprite(0), m_HasWorldBounds(0), m_HasMove(0) { }
+    ~GameObject() {}
+    
+    std::string m_Name;
+    // data for all components
+    PositionComponent m_Position;
+    SpriteComponent m_Sprite;
+    WorldBoundsComponent m_WorldBounds;
+    MoveComponent m_Move;
+    // flags for every component, indicating whether this object "has it"
+    int m_HasPosition : 1;
+    int m_HasSprite : 1;
+    int m_HasWorldBounds : 1;
+    int m_HasMove : 1;
+};
+
+
+// The "scene": array of game objects.
+// "ID" of a game object is just an index into the scene array.
+typedef size_t EntityID;
+typedef std::vector<GameObject*> GameObjectVector;
+static GameObjectVector s_Objects;
+
+
+// -------------------------------------------------------------------------------------------------
+// "systems" that we have; they operate on components of game objects
+
 
 struct MoveSystem
 {
-    WorldBoundsComponent* bounds;
-    std::vector<PositionComponent*> positionList;
-    std::vector<MoveComponent*> moveList;
+    EntityID boundsID; // ID if object with world bounds
+    std::vector<EntityID> entities; // IDs of objects that should be moved
 
-    void AddObjectToSystem(MoveComponent* o)
+    void AddObjectToSystem(EntityID id)
     {
-        positionList.emplace_back(o->GetGameObject().GetComponent<PositionComponent>());
-        moveList.emplace_back(o);
+        entities.emplace_back(id);
     }
 
-    void Initialize()
+    void SetBounds(EntityID id)
     {
-        bounds = FindOfType<WorldBoundsComponent>();
+        boundsID = id;
     }
     
     void UpdateSystem(double time, float deltaTime)
     {
+        const WorldBoundsComponent* bounds = &s_Objects[boundsID]->m_WorldBounds;
+
         // go through all the objects
-        for (size_t io = 0, no = positionList.size(); io != no; ++io)
+        for (size_t io = 0, no = entities.size(); io != no; ++io)
         {
-            PositionComponent* pos = positionList[io];
-            MoveComponent* move = moveList[io];
+            PositionComponent* pos = &s_Objects[io]->m_Position;
+            MoveComponent* move = &s_Objects[io]->m_Move;
             
             // update position based on movement velocity & delta time
             pos->x += move->velx * deltaTime;
@@ -240,35 +148,30 @@ struct MoveSystem
 
 static MoveSystem s_MoveSystem;
 
-void MoveComponent::Start()
-{
-    s_MoveSystem.AddObjectToSystem(this);
-}
 
 
-
-// "Avoidance system" works out interactions between objects that have AvoidThis and Avoid
-// components. Objects with Avoid component:
-// - when they get closer to AvoidThis than AvoidThis::distance, they bounce back,
+// "Avoidance system" works out interactions between objects that "avoid" and "should be avoided".
+// Objects that avoid:
+// - when they get closer to things that should be avoided than the given distance, they bounce back,
 // - also they take sprite color from the object they just bumped into
 struct AvoidanceSystem
 {
-    // things to be avoided: distances to them, and their position components
+    // things to be avoided: distances to them, and their IDs
     std::vector<float> avoidDistanceList;
-    std::vector<PositionComponent*> avoidPositionList;
+    std::vector<EntityID> avoidList;
     
-    // objects that avoid: their position components
-    std::vector<PositionComponent*> objectList;
-    
-    void AddAvoidThisObjectToSystem(PositionComponent* pos, float distance)
+    // objects that avoid: their IDs
+    std::vector<EntityID> objectList;
+
+    void AddAvoidThisObjectToSystem(EntityID id, float distance)
     {
+        avoidList.emplace_back(id);
         avoidDistanceList.emplace_back(distance);
-        avoidPositionList.emplace_back(pos);
     }
     
-    void AddObjectToSystem(PositionComponent* pos)
+    void AddObjectToSystem(EntityID id)
     {
-        objectList.emplace_back(pos);
+        objectList.emplace_back(id);
     }
     
     static float DistanceSq(const PositionComponent* a, const PositionComponent* b)
@@ -278,9 +181,11 @@ struct AvoidanceSystem
         return dx * dx + dy * dy;
     }
     
-    void ResolveCollision(PositionComponent* pos, float deltaTime)
+    void ResolveCollision(GameObject& go, float deltaTime)
     {
-        MoveComponent* move = pos->GetGameObject().GetComponent<MoveComponent>();
+        PositionComponent* pos = &go.m_Position;
+        MoveComponent* move = &go.m_Move;
+
         // flip velocity
         move->velx = -move->velx;
         move->vely = -move->vely;
@@ -295,22 +200,24 @@ struct AvoidanceSystem
         // go through all the objects
         for (size_t io = 0, no = objectList.size(); io != no; ++io)
         {
-            PositionComponent* myposition = objectList[io];
+            GameObject& go = *s_Objects[objectList[io]];
+            PositionComponent* myposition = &go.m_Position;
 
             // check each thing in avoid list
-            for (size_t ia = 0, na = avoidPositionList.size(); ia != na; ++ia)
+            for (size_t ia = 0, na = avoidList.size(); ia != na; ++ia)
             {
                 float avDistance = avoidDistanceList[ia];
-                PositionComponent* avoidposition = avoidPositionList[ia];
+                GameObject& avoid = *s_Objects[avoidList[ia]];
+                PositionComponent* avoidposition = &avoid.m_Position;
                 
                 // is our position closer to "thing to avoid" position than the avoid distance?
                 if (DistanceSq(myposition, avoidposition) < avDistance * avDistance)
                 {
-                    ResolveCollision(myposition, deltaTime);
+                    ResolveCollision(go, deltaTime);
                     
                     // also make our sprite take the color of the thing we just bumped into
-                    SpriteComponent* avoidSprite = avoidposition->GetGameObject().GetComponent<SpriteComponent>();
-                    SpriteComponent* mySprite = myposition->GetGameObject().GetComponent<SpriteComponent>();
+                    SpriteComponent* avoidSprite = &avoid.m_Sprite;
+                    SpriteComponent* mySprite = &go.m_Sprite;
                     mySprite->colorR = avoidSprite->colorR;
                     mySprite->colorG = avoidSprite->colorG;
                     mySprite->colorB = avoidSprite->colorB;
@@ -322,6 +229,7 @@ struct AvoidanceSystem
 
 static AvoidanceSystem s_AvoidanceSystem;
 
+
 // -------------------------------------------------------------------------------------------------
 // "the game"
 
@@ -329,17 +237,18 @@ static AvoidanceSystem s_AvoidanceSystem;
 extern "C" void game_initialize(void)
 {
     // create "world bounds" object
+    WorldBoundsComponent bounds;
     {
         GameObject* go = new GameObject("bounds");
-        WorldBoundsComponent* bounds = new WorldBoundsComponent();
-        bounds->xMin = -80.0f;
-        bounds->xMax =  80.0f;
-        bounds->yMin = -50.0f;
-        bounds->yMax =  50.0f;
-        go->AddComponent(bounds);
+        go->m_WorldBounds.xMin = -80.0f;
+        go->m_WorldBounds.xMax =  80.0f;
+        go->m_WorldBounds.yMin = -50.0f;
+        go->m_WorldBounds.yMax =  50.0f;
+        bounds = go->m_WorldBounds;
+        go->m_HasWorldBounds = 1;
+        s_MoveSystem.SetBounds(s_Objects.size());
         s_Objects.emplace_back(go);
     }
-    WorldBoundsComponent* bounds = FindOfType<WorldBoundsComponent>();
     
     // create regular objects that move
     for (auto i = 0; i < kObjectCount; ++i)
@@ -347,26 +256,25 @@ extern "C" void game_initialize(void)
         GameObject* go = new GameObject("object");
 
         // position it within world bounds
-        PositionComponent* pos = new PositionComponent();
-        pos->x = RandomFloat(bounds->xMin, bounds->xMax);
-        pos->y = RandomFloat(bounds->yMin, bounds->yMax);
-        go->AddComponent(pos);
+        go->m_Position.x = RandomFloat(bounds.xMin, bounds.xMax);
+        go->m_Position.y = RandomFloat(bounds.yMin, bounds.yMax);
+        go->m_HasPosition = 1;
 
         // setup a sprite for it (random sprite index from first 5), and initial white color
-        SpriteComponent* sprite = new SpriteComponent();
-        sprite->colorR = 1.0f;
-        sprite->colorG = 1.0f;
-        sprite->colorB = 1.0f;
-        sprite->spriteIndex = rand() % 5;
-        sprite->scale = 1.0f;
-        go->AddComponent(sprite);
+        go->m_Sprite.colorR = 1.0f;
+        go->m_Sprite.colorG = 1.0f;
+        go->m_Sprite.colorB = 1.0f;
+        go->m_Sprite.spriteIndex = rand() % 5;
+        go->m_Sprite.scale = 1.0f;
+        go->m_HasSprite = 1;
 
         // make it move
-        MoveComponent* move = new MoveComponent(0.5f, 0.7f);
-        go->AddComponent(move);
+        go->m_Move.Initialize(0.5f, 0.7f);
+        go->m_HasMove = 1;
+        s_MoveSystem.AddObjectToSystem(s_Objects.size());
 
         // make it avoid the bubble things, by adding to the avoidance system
-        s_AvoidanceSystem.AddObjectToSystem(pos);
+        s_AvoidanceSystem.AddObjectToSystem(s_Objects.size());
 
         s_Objects.emplace_back(go);
     }
@@ -377,37 +285,27 @@ extern "C" void game_initialize(void)
         GameObject* go = new GameObject("toavoid");
         
         // position it in small area near center of world bounds
-        PositionComponent* pos = new PositionComponent();
-        pos->x = RandomFloat(bounds->xMin, bounds->xMax) * 0.2f;
-        pos->y = RandomFloat(bounds->yMin, bounds->yMax) * 0.2f;
-        go->AddComponent(pos);
+        go->m_Position.x = RandomFloat(bounds.xMin, bounds.xMax) * 0.2f;
+        go->m_Position.y = RandomFloat(bounds.yMin, bounds.yMax) * 0.2f;
+        go->m_HasPosition = 1;
 
         // setup a sprite for it (6th one), and a random color
-        SpriteComponent* sprite = new SpriteComponent();
-        sprite->colorR = RandomFloat(0.5f, 1.0f);
-        sprite->colorG = RandomFloat(0.5f, 1.0f);
-        sprite->colorB = RandomFloat(0.5f, 1.0f);
-        sprite->spriteIndex = 5;
-        sprite->scale = 2.0f;
-        go->AddComponent(sprite);
+        go->m_Sprite.colorR = RandomFloat(0.5f, 1.0f);
+        go->m_Sprite.colorG = RandomFloat(0.5f, 1.0f);
+        go->m_Sprite.colorB = RandomFloat(0.5f, 1.0f);
+        go->m_Sprite.spriteIndex = 5;
+        go->m_Sprite.scale = 2.0f;
+        go->m_HasSprite = 1;
         
         // make it move, slowly
-        MoveComponent* move = new MoveComponent(0.1f, 0.2f);
-        go->AddComponent(move);
-        
+        go->m_Move.Initialize(0.1f, 0.2f);
+        go->m_HasMove = 1;
+        s_MoveSystem.AddObjectToSystem(s_Objects.size());
+
         // add to avoidance this as "Avoid This" object
-        s_AvoidanceSystem.AddAvoidThisObjectToSystem(pos, 1.3f);
+        s_AvoidanceSystem.AddAvoidThisObjectToSystem(s_Objects.size(), 1.3f);
         
         s_Objects.emplace_back(go);
-    }
-    
-    // initialize systems
-    s_MoveSystem.Initialize();
-
-    // call Start on all objects/components once they are all created
-    for (auto go : s_Objects)
-    {
-        go->Start();
     }
 }
 
@@ -437,18 +335,16 @@ extern "C" int game_update(sprite_data_t* data, double time, float deltaTime)
         //
         // Using a smaller global scale "zooms out" the rendering, so to speak.
         float globalScale = 0.05f;
-        PositionComponent* pos = go->GetComponent<PositionComponent>();
-        SpriteComponent* sprite = go->GetComponent<SpriteComponent>();
-        if (pos != nullptr && sprite != nullptr)
+        if (go->m_HasPosition && go->m_HasSprite)
         {
             sprite_data_t& spr = data[objectCount++];
-            spr.posX = pos->x * globalScale;
-            spr.posY = pos->y * globalScale;
-            spr.scale = sprite->scale * globalScale;
-            spr.colR = sprite->colorR;
-            spr.colG = sprite->colorG;
-            spr.colB = sprite->colorB;
-            spr.sprite = (float)sprite->spriteIndex;
+            spr.posX = go->m_Position.x * globalScale;
+            spr.posY = go->m_Position.y * globalScale;
+            spr.scale = go->m_Sprite.scale * globalScale;
+            spr.colR = go->m_Sprite.colorR;
+            spr.colG = go->m_Sprite.colorG;
+            spr.colB = go->m_Sprite.colorB;
+            spr.sprite = (float)go->m_Sprite.spriteIndex;
         }
     }
     return objectCount;
